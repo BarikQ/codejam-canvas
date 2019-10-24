dc3d9db7084b2b02c4aa0ad6054d7459c52d199c
BarikQ <yar.barik99@gmail.com>
2019-10-24 16:40:04 +0300

    feat: 4x4 & 32x32 pictures

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,13 +3,12 @@ const MEDIUM_URL = `https://raw.githubusercontent.com/rolling-scopes-school/task
 const BIG_URL = `https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png`;
 
 (function main() {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-
-
+  let canvas = document.querySelector('#canvas');
+  let ctx = canvas.getContext('2d');
+  
   ctx.imageSmoothingEnabled = false;
-  canvas.width = 256;
-  canvas.height = 256;
+  canvas.width = parseInt(document.querySelector('.active').id);
+  canvas.height = parseInt(document.querySelector('.active').id);
 
   const sizeItems = document.querySelectorAll('.size-item');
 
@@ -17,7 +16,7 @@ const BIG_URL = `https://raw.githubusercontent.com/rolling-scopes-school/tasks/m
     elem.addEventListener('click', (event) => changeSize(elem, sizeItems, canvas, event));
   })
 
-  reDraw(canvas, BIG_URL);
+  reDraw(canvas, MEDIUM_URL);
 })();
 
 function changeSize(elem, sizeItems, canvas, event) {
@@ -30,6 +29,8 @@ function changeSize(elem, sizeItems, canvas, event) {
     })
     elem.classList.add('active');
   }
+  canvas.width = newSize;
+  canvas.height = newSize;
 
   switch (newSize) {
     case 4:
@@ -42,31 +43,45 @@ function changeSize(elem, sizeItems, canvas, event) {
       reDraw(canvas, BIG_URL);
       break;
     default:
-      console.log('gg');
+      console.log('error');
   }
-
-  // canvas.width = newSize;
-  // canvas.height = newSize;
 }
 
 function reDraw(canvas, url) {
-  const ctx = canvas.getContext('2d');
+  let ctx = canvas.getContext('2d');
+  let dummyImage = document.querySelector('#test');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'red';
-  ctx.fillRect(0, 0, 50, 50);
+
   if (url === BIG_URL) {
     fetch(url)
     .then(response => response.blob())
     .then(image => {
       const SRC = URL.createObjectURL(image);
-      document.querySelector('#test').src = SRC;
-  });
+      dummyImage.src = SRC;
+      return dummyImage;
+    })
+    .then(image => ctx.drawImage(image, 0, 0));
   } else {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      data.forEach((elem, i) => {
+        elem.forEach((color, j) => {
+          if (url === SMALL_URL) {
+            ctx.fillStyle = `#${color}`;
+          } else {
+            let rgbColor = `rgba(`;
+            color.forEach((rgb, index) => {
+              if (index !== 3) rgbColor += `${rgb},`;
+              else {
+                rgbColor += `${rgb})`;
+              }
+            });
+            ctx.fillStyle = rgbColor;
+          }
+          ctx.fillRect(i, j, 1, 1);
+        });
+      });
   });
   }
-  // console.log(data);
 }
