ccc5323a95e116b280caee9ba739ebbc844e83f2
BarikQ <yar.barik99@gmail.com>
2019-10-24 15:52:14 +0300

    feat: add server requests

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,12 @@
+const SMALL_URL = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+const MEDIUM_URL = `https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json`;
+const BIG_URL = `https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png`;
+
 (function main() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
+
+
   ctx.imageSmoothingEnabled = false;
   canvas.width = 256;
   canvas.height = 256;
@@ -8,16 +14,15 @@
   const sizeItems = document.querySelectorAll('.size-item');
 
   sizeItems.forEach(elem => {
-    elem.addEventListener('click', (event) => setSize(elem, sizeItems, canvas, event));
+    elem.addEventListener('click', (event) => changeSize(elem, sizeItems, canvas, event));
   })
 
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0, 0, 50, 50);
+  reDraw(canvas, BIG_URL);
 })();
 
-function setSize(elem, sizeItems, canvas, event) {
+function changeSize(elem, sizeItems, canvas, event) {
   const newSize = parseInt(event.target.id);
-  console.log(elem, sizeItems);
+
   if (elem.classList.contains('active')) return;
   else {
     sizeItems.forEach(element => {
@@ -25,7 +30,43 @@ function setSize(elem, sizeItems, canvas, event) {
     })
     elem.classList.add('active');
   }
-  
-  canvas.width = newSize;
-  canvas.height = newSize;
+
+  switch (newSize) {
+    case 4:
+      reDraw(canvas, SMALL_URL);
+      break;
+    case 32:
+      reDraw(canvas, MEDIUM_URL);
+      break;
+    case 256:
+      reDraw(canvas, BIG_URL);
+      break;
+    default:
+      console.log('gg');
+  }
+
+  // canvas.width = newSize;
+  // canvas.height = newSize;
+}
+
+function reDraw(canvas, url) {
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, 50, 50);
+  if (url === BIG_URL) {
+    fetch(url)
+    .then(response => response.blob())
+    .then(image => {
+      const SRC = URL.createObjectURL(image);
+      document.querySelector('#test').src = SRC;
+  });
+  } else {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+  });
+  }
+  // console.log(data);
 }
