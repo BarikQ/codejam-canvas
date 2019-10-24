17e0b23152877bec585f963e32fa1c9605dff522
BarikQ <yar.barik99@gmail.com>
2019-10-24 14:09:12 +0300

    feat: add size changer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,31 @@
 (function main() {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
+  ctx.imageSmoothingEnabled = false;
+  canvas.width = 256;
+  canvas.height = 256;
+
+  const sizeItems = document.querySelectorAll('.size-item');
+
+  sizeItems.forEach(elem => {
+    elem.addEventListener('click', (event) => setSize(elem, sizeItems, canvas, event));
+  })
 
   ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 150, 100);
+  ctx.fillRect(0, 0, 50, 50);
 })();
+
+function setSize(elem, sizeItems, canvas, event) {
+  const newSize = parseInt(event.target.id);
+  console.log(elem, sizeItems);
+  if (elem.classList.contains('active')) return;
+  else {
+    sizeItems.forEach(element => {
+      if (element.classList.contains('active')) element.classList.remove('active');
+    })
+    elem.classList.add('active');
+  }
+  
+  canvas.width = newSize;
+  canvas.height = newSize;
+}
