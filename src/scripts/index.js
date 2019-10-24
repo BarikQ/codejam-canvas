c592f50d5476f35dce4a84f5ca968ef48c9d3140
BarikQ <yar.barik99@gmail.com>
2019-10-24 17:00:11 +0300

    feat: add 256x256 support

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,7 @@ const BIG_URL = `https://raw.githubusercontent.com/rolling-scopes-school/tasks/m
 (function main() {
   let canvas = document.querySelector('#canvas');
   let ctx = canvas.getContext('2d');
-  
+
   ctx.imageSmoothingEnabled = false;
   canvas.width = parseInt(document.querySelector('.active').id);
   canvas.height = parseInt(document.querySelector('.active').id);
@@ -49,39 +49,43 @@ function changeSize(elem, sizeItems, canvas, event) {
 
 function reDraw(canvas, url) {
   let ctx = canvas.getContext('2d');
-  let dummyImage = document.querySelector('#test');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   if (url === BIG_URL) {
+    const IMAGE = new Image();
     fetch(url)
-    .then(response => response.blob())
-    .then(image => {
-      const SRC = URL.createObjectURL(image);
-      dummyImage.src = SRC;
-      return dummyImage;
-    })
-    .then(image => ctx.drawImage(image, 0, 0));
+      .then(response => response.blob())
+      .then(data => URL.createObjectURL(data))
+      .then(imageSrc => {
+        IMAGE.src = imageSrc
+        return IMAGE;
+      })
+      .then(image => {
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0);
+        }
+      });
   } else {
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      data.forEach((elem, i) => {
-        elem.forEach((color, j) => {
-          if (url === SMALL_URL) {
-            ctx.fillStyle = `#${color}`;
-          } else {
-            let rgbColor = `rgba(`;
-            color.forEach((rgb, index) => {
-              if (index !== 3) rgbColor += `${rgb},`;
-              else {
-                rgbColor += `${rgb})`;
-              }
-            });
-            ctx.fillStyle = rgbColor;
-          }
-          ctx.fillRect(i, j, 1, 1);
+      .then(response => response.json())
+      .then(data => {
+        data.forEach((elem, i) => {
+          elem.forEach((color, j) => {
+            if (url === SMALL_URL) {
+              ctx.fillStyle = `#${color}`;
+            } else {
+              let rgbColor = `rgba(`;
+              color.forEach((rgb, index) => {
+                if (index !== 3) rgbColor += `${rgb},`;
+                else {
+                  rgbColor += `${rgb})`;
+                }
+              });
+              ctx.fillStyle = rgbColor;
+            }
+            ctx.fillRect(i, j, 1, 1);
+          });
         });
       });
-  });
   }
 }
